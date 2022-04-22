f1fa0539d7a41e77df002f475469d9cb6e19d3c4
seungmin-dev <seungmin.dev@gmail.com>
2022-04-22 14:29:40 +0900

    cross board movement

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const onDragEnd = (info: DropResult) => {
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       // same board movement.
       setToDos((allBoards) => {
@@ -34,6 +35,20 @@ function App() {
         boardCopy.splice(source.index, 1);
         boardCopy.splice(destination.index, 0, draggableId);
         return { ...allBoards, [source.droppableId]: boardCopy };
+      });
+    }
+    if (destination?.droppableId !== source.droppableId) {
+      // cross board movement.
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
       });
     }
   };
