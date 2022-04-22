b0d7957600dabbd8beb568cf3346c46f5121c1d0
seungmin-dev <seungmin.dev@gmail.com>
2022-04-22 14:13:17 +0900

    same board movement

---
+++ src/Components/Board.tsx
@@ -0,0 +1,40 @@
+import { Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import DraggableCard from "./DraggableCard";
+
+const Wrapper = styled.div`
+  padding: 20px 10px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Title = styled.h3`
+  text-align: center;
+  padding-bottom: 20px;
+  font-size: 20px;
+`;
+
+interface IBoardProps {
+  toDos: string[];
+  boardId: string;
+}
+function Board({ toDos, boardId }: IBoardProps) {
+  return (
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(magic) => (
+          <div ref={magic.innerRef} {...magic.droppableProps}>
+            {toDos.map((toDo, index) => (
+              <DraggableCard key={toDo} toDo={toDo} index={index} />
+            ))}
+            {magic.placeholder}{" "}
+          </div>
+        )}
+      </Droppable>
+    </Wrapper>
+  );
+}
+
+export default Board;
